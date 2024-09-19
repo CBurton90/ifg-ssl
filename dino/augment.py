dda768a3a4880f4ce64ba310e9e1ed010543161d
conradb <conradb@akatea.gns.cri.nz>
2024-09-19 15:44:32 +1200

    added hephaestus gpu training script

--- dino/augment.py
+++ dino/augment.py
@@ -2,6 +2,7 @@ from PIL import Image
 
 import torch
 from torchvision import transforms
+from torchvision.transforms import v2
 
 import dino.utils as utils
 
@@ -40,6 +41,55 @@ class ImageAugmentationDINO(object):
         self.local_transfo = transforms.Compose([
             transforms.RandomResizedCrop(96, scale=local_crops_scale, interpolation=Image.BICUBIC), #96 crop
             flip_and_color_jitter,
+            utils.GaussianBlur(p=0.5),
+            normalize,
+            ])
+        
+    def __call__(self, image):
+        crops = []
+        crops.append(self.global_transfo1(image))
+        crops.append(self.global_transfo2(image))
+        for _ in range(self.local_crops_number):
+            crops.append(self.local_transfo(image))
+        return crops
+    
+class IfgAugmentationDINO(object):
+    def __init__(self, global_crops_scale, local_crops_scale, local_crops_number):
+        flip_and_elastictf = transforms.Compose([
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.5),
+            transforms.ElasticTransform(alpha=25.0),
+            # transforms.RandomApply([transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)], p=0.8),
+            # transforms.RandomGrayscale(p=0.2),
+            ])
+        
+        normalize = transforms.Compose([
+            transforms.ToTensor(),
+            # transforms.Normalize((0.6864, 0.5986, 0.5747), (0.3004, 0.2943, 0.2855)), #Hesphaestus Interferogram means + std's for train dataset mean: tensor([0.6864, 0.5986, 0.5747]) std: tensor([0.3004, 0.2943, 0.2855])
+            ])
+        
+        # first global crop
+        self.global_transfo1 = transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC), #224 crop
+            flip_and_elastictf,
+            utils.GaussianBlur(1.0),
+            normalize,
+            ])
+        
+         # second global crop (with Gaussian Blur + Noise)
+        self.global_transfo2 = transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC), #224 crop
+            flip_and_elastictf,
+            utils.GaussianBlur(0.1),
+            # v2.GaussianNoise(sigma=0.1),
+            normalize,
+            ])
+
+        # transformation for the local small crops
+        self.local_crops_number = local_crops_number
+        self.local_transfo = transforms.Compose([
+            transforms.RandomResizedCrop(96, scale=local_crops_scale, interpolation=Image.BICUBIC), #96 crop
+            flip_and_elastictf,
             utils.GaussianBlur(p=0.5),
             normalize,
             ])
---
+++ hephaestus_dino_gpu.py
@@ -0,0 +1,197 @@
+# standard python imports
+import sys
+sys.path.append("/home/conradb/git/ifg-ssl")
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import tqdm as tqdm
+import math
+
+# pytorch + fastai imports
+from fastai.vision.all import *
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import torchvision.transforms.functional as F
+from torchvision.utils import make_grid
+
+# local module imports
+from dino.augment import IfgAugmentationDINO
+from dino.loss import DINOLoss
+import dino.utils as utils
+import dino.vision_transformer as vit
+from dino.vision_transformer import DINOHead
+
+# augmentations
+global_crops_scale = (0.4, 1.)
+#global_crops_scale = (0.25, 1.)
+local_crops_scale = (0.05, 0.4)
+#local_crops_scale = (0.05, 0.25)
+#local_crops_number = 8
+local_crops_number = 8
+
+# dataloader
+batch_size = 256 #gpu
+num_workers = 32
+pin_memory = True #gpu
+
+# model
+model = 'vit_small' # embed_dim=192, depth=12, num_heads=3
+out_dim = 16384 # complex and large datasets values like 65k work well
+use_bn_head = False # use batch norm in head
+#norm_last_layer = True #  normalize the last layer of the DINO head, typically set this paramater to False with vit_small and True with vit_base
+norm_last_layer = True
+
+# dino specific
+warmup_teacher_temp = 0.03 # Initial value for the teacher temperature, Try decreasing it if the training loss does not decrease
+#warmup_teacher_temp = 0.02
+teacher_temp = 0.04
+#teacher_temp = 0.07
+warmup_teacher_temp_epochs = 30 # 30 is default?
+
+# compute
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using {device}')
+
+epochs = 301
+#lr = 0.0005
+lr = 1e-3
+#min_lr = 1e-6
+min_lr= 1e-5
+warmup_epochs = 10
+weight_decay = 0.04
+weight_decay_end = 0.4
+#momentum_teacher = 0.996 # Base EMA parameter for teacher update. The value is increased to 1 during training with cosine schedule. We recommend setting a higher value with small batches: for example use 0.9995 with batch size of 256.""")
+momentum_teacher = 0.99
+
+
+transform = IfgAugmentationDINO(global_crops_scale, local_crops_scale, local_crops_number)
+dataset = datasets.ImageFolder(root='/scratch/SDF25/Hephaestus_Classification/InSARLabelled', transform=transform)
+
+# Stratified Sampling for train and val
+train_idx, validation_idx = train_test_split(np.arange(len(dataset)),
+                                             test_size=0.2,
+                                             random_state=999,
+                                             shuffle=True,
+                                             stratify=dataset.targets)
+
+# Subset dataset for train and val
+train_dataset = Subset(dataset, train_idx)
+validation_dataset = Subset(dataset, validation_idx)
+
+# Weighted sampling to address class imbalance
+targets = [dataset.targets[i] for i in train_idx]
+c = Counter(targets)
+c = OrderedDict(sorted(c.items()))
+print(c)
+length = 0
+for value in c.values():
+    length += int(value)
+assert length == len(train_dataset)
+
+# calcuate weight of each class
+class_weights = [1.0/n for n in c.values()]
+print(class_weights)
+
+# assign weight to each sample
+sample_weights = [class_weights[n] for n in targets]
+
+# Create WeightedRandomSampler
+sampler = WeightedRandomSampler(sample_weights, len(sample_weights))
+
+# assign sampler
+train_dataloader = DataLoader(train_dataset, sampler=sampler, batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory, drop_last=True)
+
+student = vit.__dict__[model](patch_size=16, drop_path_rate=0.1)
+teacher = vit.__dict__[model](patch_size=16)
+embed_dim = student.embed_dim
+
+# multi-crop wrapper handles forward with inputs of different resolutions
+student = utils.MultiCropWrapper(student, DINOHead(embed_dim, out_dim, use_bn=use_bn_head, norm_last_layer=norm_last_layer))
+teacher = utils.MultiCropWrapper(teacher, DINOHead(embed_dim, out_dim, use_bn=use_bn_head))
+teacher.load_state_dict(student.state_dict())
+
+# there is no backpropagation through the teacher, so no need for gradients
+for p in teacher.parameters():
+    p.requires_grad = False
+
+dino_loss = DINOLoss(
+        out_dim,
+        local_crops_number + 2,  # total number of crops = 2 global crops + local_crops_number
+        warmup_teacher_temp,
+        teacher_temp,
+        warmup_teacher_temp_epochs,
+        epochs,
+        ).to(device)
+
+params_groups = utils.get_params_groups(student)
+optimizer = torch.optim.AdamW(params_groups)  # to use with ViTs
+
+lr_schedule = utils.cosine_scheduler(
+        lr,  # linear scaling rule
+        min_lr, epochs, len(train_dataloader),
+        warmup_epochs=warmup_epochs)
+
+wd_schedule = utils.cosine_scheduler(weight_decay, weight_decay_end, epochs, len(train_dataloader))
+
+# momentum parameter is increased to 1. during training with a cosine schedule
+momentum_schedule = utils.cosine_scheduler(momentum_teacher, 1, epochs, len(train_dataloader))
+
+for epoch in range(0, epochs):
+
+    running_loss = 0
+    counts = 0
+
+    for itr, (batch, _) in enumerate(train_dataloader):
+
+        images = [image.to(device) for image in batch]
+
+        it = len(train_dataloader) * epoch + itr  # global training iteration
+
+        for i, param_group in enumerate(optimizer.param_groups):
+            param_group["lr"] = lr_schedule[it]
+            if i == 0:  # only the first group is regularized
+                param_group["weight_decay"] = wd_schedule[it]
+
+        # teacher and student forward passes + compute dino loss
+        teacher_output = teacher(images[:2])  # only the 2 global views pass through the teacher
+        student_output = student(images)
+        loss = dino_loss(student_output, teacher_output, epoch)
+
+        running_loss += loss
+        counts += 1
+
+        if not math.isfinite(loss.item()):
+            print("Loss is {}, stopping training".format(loss.item()), force=True)
+            sys.exit(1)
+
+        # student update
+        optimizer.zero_grad()
+        param_norms = None
+        loss.backward()
+
+        #param_norms = utils.clip_gradients(student, 3.0)
+        utils.cancel_gradients_last_layer(epoch, student, 1)
+        optimizer.step()
+
+        # EMA update for the teacher
+        with torch.no_grad():
+            m = momentum_schedule[it]  # momentum parameter
+            for param_q, param_k in zip(student.parameters(), teacher.parameters()):
+                param_k.data.mul_(m).add_((1 - m) * param_q.detach().data)
+                
+        if itr == 0:
+            if epoch % 5 == 0:
+                
+                save_dict = {
+                        'student': student.state_dict(),
+                        'teacher': teacher.state_dict(),
+                        'optimizer': optimizer.state_dict(),
+                        'epoch': epoch + 1,
+                        'dino_loss': dino_loss.state_dict(),
+                        }
+                print('saving checkpoint')
+                torch.save(save_dict, 'dino/dino_checkpoints/hephaestus_ViTS16_ckpt_v1.pth')
+
+    epoch_loss = running_loss / counts
+    print(f'Epoch {epoch} loss is {epoch_loss}')
